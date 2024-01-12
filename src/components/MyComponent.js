9451ce55cee22b128d53bbe3f1c6e684426ec37d
diep nguyen <diepmbacc@gmail.com>
2024-01-12 11:12:05 +0700

    Video 18

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,13 +6,19 @@
 import React from "react";
 
 class MyComponent extends React.Component {
-    
+     
+    state = {
+        name: 'Jackn',
+        age: 26,
+        address: 'Ninh xuan, Hoa lu , Ninh binh'
+    }
+
     // JSX
     render(){
         return (
-            <div> My first component
-
-                {Math.random()}
+            <div> 
+                  My name is {this.state.name}
+                  I am {this.state.age} years old
             </div>
         ) ;    
     }
